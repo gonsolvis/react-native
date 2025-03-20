37e7fd92cbab4f7e7f61ff649e4a92ad8896c5f5
ross <gonsolvis@gmail.com>
2025-03-20 01:16:41 +0100

    first commit

---
+++ app/movie/[id].jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function MovieInfo() {
+  return (
+    <View>
+      <Text>MovieInfo</Text>
+    </View>
+  )
+}
